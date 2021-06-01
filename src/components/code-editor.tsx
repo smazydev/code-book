048ab0ac75796ad3d08b267152bec842adbb3db3
smazydev <alizafar1437@gmail.com>
2021-06-01 14:52:12 -0400

    added configuration options for monaco editor according to project requirements

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,23 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-    return <MonacoEditor theme="vs-dark" language={"javascript"} height="500px"/>;
+  return (
+    <MonacoEditor
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout:true,
+      }}
+      theme="vs-dark"
+      language="javascript"
+      height="500px"
+    />
+  );
 };
 
 export default CodeEditor;
