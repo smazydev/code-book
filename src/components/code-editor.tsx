cd815bc37d91f2e8fd30d77d4dcb4158df6b9af7
smazydev <alizafar1437@gmail.com>
2021-06-02 06:14:36 -0400

    added prettier formatting and OnFormatClick, OnEditorMount functions

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,21 +1,45 @@
 import Editor from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+import {useRef} from "react";
 
 interface ICodeEditor {
   setInput(value: string): void;
 }
 
 const CodeEditor: React.FC<ICodeEditor> = ({ setInput }) => {
+  const editorRef = useRef<any>(null); 
   
-  const handleEditorChange = (value:any, event:any) => {
+  const handleEditorChange = (value:any, event:any):void => {
     // here is the current value
     setInput(value);
   }
+  const onEditorMount = (editor:any,monaco:any) => {
+    editorRef.current = editor;
+  }
+  const onFormatClick = () => {
+    //get current value from editor.
+    const unformatted = editorRef.current.getModel().getValue();
+    //format the value
+    const formatted = prettier.format(unformatted,{
+      parser:'babel',
+      plugins:[parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    })
+    //set new formatted code to editor
+    editorRef.current.setValue(formatted);
+  }
 
   return (
+    <div>
+      <button onClick={onFormatClick}>Format</button>
     <Editor
-      height="90vh"
+      height="500px"
       defaultLanguage="javascript"
       onChange={handleEditorChange}
+      onMount={onEditorMount}
       options={{
         wordWrap: "on",
         minimap: { enabled: false },
@@ -28,6 +52,7 @@ const CodeEditor: React.FC<ICodeEditor> = ({ setInput }) => {
       }}
       theme="vs-dark"
     />
+    </div>
   );
 };
 
